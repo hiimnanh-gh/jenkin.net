faa7644b5c0bf60db3fa41c4c40df10de8db8b5a
Anh Dao Nguyen <daonguyenanhprivate@gmail.com>
2025-06-20 08:16:32 +0700

    Clean initial commit

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca91b9907f48718eda92435f956607ab99c9f133")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

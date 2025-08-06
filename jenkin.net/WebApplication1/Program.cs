33a36075b971ba36461ae4bf93d33121c1750311
peterposion <nghoang0112@gmail.com>
2025-08-06 23:30:46 +0700

    Re-add jenkin.net as normal folder

---
+++ jenkin.net/WebApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// ✅ Lọc các tham số dạng URL từ args để dùng cho UseUrls
+var urls = args.Where(arg => arg.StartsWith("http://") || arg.StartsWith("https://")).ToArray();
+builder.WebHost.UseUrls(urls);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.WebHost.UseUrls("http://0.0.0.0:80");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
